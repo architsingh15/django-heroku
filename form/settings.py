205468ada3f19c59404826431bc3b5b88dadb1d4
Archit Singh <archit.singh@hashedin.com>
2018-10-10 22:13:43 +0530

    changed static_root url

--- form/settings.py
+++ form/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
